aaaa5b1bb43ddb2b1049e4ff46ebbef26b9828d1
vzdrizhni <vzdrizhni@gmail.com>
2020-09-07 23:39:46 +0500

    Test the service

--- src/services/__TEST__/services.test.js
+++ src/services/__TEST__/services.test.js
@@ -1,12 +1,22 @@
 import MarvelHeroes from '../marvel-api-client'
 import { array } from 'prop-types';
-import "regenerator-runtime";
 import 'babel-polyfill';
 
 const heroes = new MarvelHeroes();
+
+it('should be instance of MarvelHeroes class', () => {
+    expect(heroes).toBeInstanceOf(MarvelHeroes)
+});
+
 
 it('should return array of objects', () => {
     heroes.getAllCharacters()
     .then(resp =>
         expect(resp).toBe(array))
+});
+
+it('should return first arrays element name ', () => {
+    heroes.getAllCharacters()
+    .then(resp =>
+        expect(resp[0].name).toBe('3-D Man'))
 });
