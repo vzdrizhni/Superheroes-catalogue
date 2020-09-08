2a9a96e5104af835d35ae4918f7305ac0f15dd2d
vzdrizhni <vzdrizhni@gmail.com>
2020-09-08 14:31:56 +0500

    Updates

---
+++ precache-manifest.afae921e5751afceca3b5af7dfd055a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74fc0a7cdeb91cc3bad94fa0cbdabc6",
+    "url": "/Superheroes-catalogue/index.html"
+  },
+  {
+    "revision": "be63dc6cc1000cf3e480",
+    "url": "/Superheroes-catalogue/static/css/main.16a7d01c.chunk.css"
+  },
+  {
+    "revision": "5e6c02c45b1e2d6ba5ca",
+    "url": "/Superheroes-catalogue/static/js/2.4202e31c.chunk.js"
+  },
+  {
+    "revision": "c18ad82b3be9985ffcbc3de3c04c0198",
+    "url": "/Superheroes-catalogue/static/js/2.4202e31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be63dc6cc1000cf3e480",
+    "url": "/Superheroes-catalogue/static/js/main.b2bf856a.chunk.js"
+  },
+  {
+    "revision": "3c0d727224f292686ba6",
+    "url": "/Superheroes-catalogue/static/js/runtime-main.2eaa01da.js"
+  },
+  {
+    "revision": "78f339d10d0a8b7d84dc5d4e797e4971",
+    "url": "/Superheroes-catalogue/static/media/Marvel-Logo.78f339d1.png"
+  }
+]);
